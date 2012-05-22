650e892567082a58043f2a4a9b2b227edc99da87
rvcoutinho <rvcoutinho@gmail.com>
2012-05-21 23:47:48 -0300

    - Creating tests for the test framework.  - Setting code templates, formatting and cleaning up rules;

--- components/Test/src/org/dejava/component/util/test/test/MultiDataJUnitRunnerTest.java
+++ components/Test/src/org/dejava/component/util/test/test/MultiDataJUnitRunnerTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 import org.dejava.component.util.test.runner.MultiDataJUnitRunner;
 import org.dejava.component.util.test.test.auxiliary.FakeTest;
-import org.dejava.component.util.test.test.auxiliary.SuccessRunListener;
+import org.dejava.component.util.test.test.auxiliary.RunListenerLog;
 import org.junit.Test;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
@@ -13,22 +13,21 @@ import org.junit.runners.model.InitializationError;
  * Test cases for the MultiDataJUnitRunner.
  */
 public class MultiDataJUnitRunnerTest extends TestCase {
-
+	
 	/**
-	 * Returns an instance of the {@link MultiDataJUnitRunner} for the
-	 * {@link FakeTest} that will be used to test the TestRunner.
+	 * Returns an instance of the {@link MultiDataJUnitRunner} for the {@link FakeTest} that will be used to
+	 * test the TestRunner.
 	 * 
-	 * @return An instance of the {@link MultiDataJUnitRunner} for the
-	 *         {@link FakeTest} that will be used to test the TestRunner.
+	 * @return An instance of the {@link MultiDataJUnitRunner} for the {@link FakeTest} that will be used to
+	 *         test the TestRunner.
 	 * @throws InitializationError
-	 *             If the test runner cannot me initialized for the
-	 *             {@link FakeTest}.
+	 *             If the test runner cannot me initialized for the {@link FakeTest}.
 	 */
 	private MultiDataJUnitRunner getFakeTestRunner() throws InitializationError {
 		// Returns a new instance of the runner for the FakeTest.
 		return new MultiDataJUnitRunner(FakeTest.class);
 	}
-
+	
 	/**
 	 * TODO
 	 * 
@@ -40,8 +39,7 @@ public class MultiDataJUnitRunnerTest extends TestCase {
 		// Creates a new notifier.
 		RunNotifier runNotifier = new RunNotifier();
 		// Creates a listener for the FakeTest.
-		SuccessRunListener runListener = new SuccessRunListener(
-				"testNormalSuccess");
+		RunListenerLog runListener = new RunListenerLog("testNormalSuccess");
 		// Adds the listener to the notifier.
 		runNotifier.addListener(runListener);
 		// Tries to run the tests.

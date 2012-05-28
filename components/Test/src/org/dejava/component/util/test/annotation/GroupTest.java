264a2110c2f328f1e3234dd117e4aec79bc9f07c
Romulo Coutinho <rvcoutinho@gmail.com>
2012-05-28 14:32:41 -0300

    - Creanting the GroupTest.java;

---
+++ components/Test/src/org/dejava/component/util/test/annotation/GroupTest.java
@@ -0,0 +1,26 @@
+package org.dejava.component.util.test.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.dejava.component.util.test.runner.ParametricJUnitRunner;
+
+/**
+ * Defines testing groups in a test class. Must be used with proper JUnit runner (as for
+ * {@link ParametricJUnitRunner})
+ */
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = { ElementType.TYPE, ElementType.METHOD })
+public @interface GroupTest {
+	
+	/**
+	 * If annotating a type: defines the groups of tests that should be considered while running the test
+	 * class. If empty (or if the annotation is not present for the class), all groups must be tested.
+	 * 
+	 * If annotating a test method: defines the groups that the test method is part of. If empty, the test
+	 * method will only be invoked if the all groups must be invoked (see definition above).
+	 */
+	String[] groups() default {};
+}

f6fb4ca9e53aef332144b94d92f150ecbcbfe04b
Romulo Coutinho <rvcoutinho@gmail.com>
2012-09-05 18:33:02 -0300

    - Configuring maven;

---
+++ components/Reflection/src/test/java/org/dejava/component/reflection/test/ClassMirrorTest.java
@@ -0,0 +1,17 @@
+package org.dejava.component.reflection.test;
+
+import org.dejava.component.test.runner.JUnitParametricRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests for the class mirror.
+ */
+@RunWith(JUnitParametricRunner.class)
+public class ClassMirrorTest {
+	
+	@Test
+	public void name() {
+		
+	}
+}

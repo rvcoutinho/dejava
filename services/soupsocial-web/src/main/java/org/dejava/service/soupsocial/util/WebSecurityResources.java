d2a0fbc8a12d4a4becba004e4a1b8041c6ca0a76
rvcoutinho <rvcoutinho@gmail.com>
2013-06-05 20:05:38 -0300

    - Recreating pages;  - Configuring shiro;  - Configuring preetyfaces;

---
+++ services/soupsocial-web/src/main/java/org/dejava/service/soupsocial/util/WebSecurityResources.java
@@ -0,0 +1,60 @@
+package org.dejava.service.soupsocial.util;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Singleton;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+
+/**
+ * Web security resources to be injected CDI.
+ */
+@Singleton
+@ManagedBean(name = "securityUtils")
+public class WebSecurityResources {
+
+	/**
+	 * The Shiro ini file path.
+	 */
+	private static final String SHIRO_INI_PATH = "classpath:shiro.ini";
+
+	/**
+	 * The security manager.
+	 */
+	private SecurityManager securityManager;
+
+	/**
+	 * Loads the security manager.
+	 */
+	@PostConstruct
+	public void loadSecurityManager() {
+		// Creates a new security manager for the ini file.
+		securityManager = new IniSecurityManagerFactory(SHIRO_INI_PATH).getInstance();
+		// Sets the security manager in the security utils.
+		SecurityUtils.setSecurityManager(securityManager);
+	}
+
+	/**
+	 * Gets the security manager for the web app.
+	 * 
+	 * @return The security manager for the web app.
+	 */
+	@Produces
+	public SecurityManager getSecurityManager() {
+		return securityManager;
+	}
+
+	/**
+	 * Gets the current subject.
+	 * 
+	 * @return The current subject.
+	 */
+	@Produces
+	public Subject getSubject() {
+		return SecurityUtils.getSubject();
+	}
+}
